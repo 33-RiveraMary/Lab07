a095cd881c68fc305d604b40e54d8e0a7e23b4f7
33-RiveraMary <114891412+33-RiveraMary@users.noreply.github.com>
2022-10-25 11:43:17 +0800

    Added Game Over conditions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
     private Rigidbody rigidBody;
 
+    //jump
     public float jumpForce;
+
+    //gameover
+    private bool isAlive;
+    public GameObject gameoverTxt;
 
     void Start()
     {
@@ -15,22 +21,49 @@ public class Player : MonoBehaviour
         thisAnimation["Flap_Legacy"].speed = 3;
 
         rigidBody = GetComponent<Rigidbody>();
+
+        isAlive = true;
+        gameoverTxt.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        //check if game is playing
+        if (isAlive)
         {
-            thisAnimation.Play();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                thisAnimation.Play();
 
-            //jump
-            rigidBody.velocity += Vector3.up * jumpForce;
+                //jump
+                rigidBody.velocity += Vector3.up * jumpForce;
+            }
         }
 
         //ensure cannot fly above screen
         if (transform.position.y > 3.55f)
         {
             transform.position = new Vector3(0f, 3.55f, 0f);
+        }
+
+        //gameover when fall under screen
+        if (transform.position.y < -3.7f)
+        {
+            //gameover
+            gameoverTxt.SetActive(true);
+
+            isAlive = false;
+        }
+    }
+
+    private void OnCollisionEnter (Collision other)
+    {
+        //gameover when collide with obstacle
+        if (other.gameObject.tag == "Obstacle")
+        {
+            gameoverTxt.SetActive(true);
+
+            isAlive = false;
         }
     }
 }
